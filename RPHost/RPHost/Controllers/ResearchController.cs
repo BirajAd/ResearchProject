8facdf9544dda8342091696e713d4376889c9c13
sandescrst <sandescrst@gmail.com>
2020-12-17 17:39:22 -0600

    testing cache

---
+++ RPHost/RPHost/Controllers/ResearchController.cs
@@ -0,0 +1,63 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RPHost.Data;
+
+namespace RPHost.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResearchController : ControllerBase
+    {
+
+        private readonly DataContext _context;
+        public ResearchController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/research
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var researches = await _context.Researches.ToListAsync();
+
+            return Ok(researches);
+        }
+
+        // GET api/research/5
+        [AllowAnonymous]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var value = await _context.Researches.FirstOrDefaultAsync(x => x.ResearchId == id);
+            return Ok(value);
+        }
+
+        // POST api/research
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+
+        }
+
+        // PUT api/research/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+
+        }
+
+        // DELETE api/research/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            
+        }
+    }
+}
---
+++ RPHost/RPHost/Helpers/UserParams.cs
@@ -0,0 +1,21 @@
+namespace RPHost.Helpers
+{
+    public class UserParams
+    {
+        private const int MaxPageSize = 50;
+        // if not requested it always return the first page(=1)
+        public int PageNumber { get; set; } = 1;
+        private int pageSize = 5;
+        public int PageSize
+        {
+            get { return pageSize; }
+            //we we have value which is greater than maxPagesize we return maxpagesize othwise we return value
+            set { pageSize = (value > MaxPageSize) ? MaxPageSize : value; }
+        }
+        
+        public int UserId { get; set; }
+        
+
+        
+    }
+}
