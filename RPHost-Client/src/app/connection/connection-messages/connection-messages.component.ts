62d81a9bc4515c784d421259ae25901e1fa2424c
biraj <adhikari.biraj1@gmail.com>
2021-06-14 23:34:47 -0500

    conversation loading completed

--- RPHost-Client/src/app/connection/connection-messages/connection-messages.component.ts
+++ RPHost-Client/src/app/connection/connection-messages/connection-messages.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Message } from 'src/app/_models/message';
+import { User } from 'src/app/_models/user';
+import { AuthService } from 'src/app/_services/auth.service';
 import { UserService } from 'src/app/_services/user.service';
 
 @Component({
@@ -7,14 +10,27 @@ import { UserService } from 'src/app/_services/user.service';
   templateUrl: './connection-messages.component.html',
   styleUrls: ['./connection-messages.component.css']
 })
-export class ConnectionMessagesComponent implements OnInit {
+export class ConnectionMessagesComponent implements OnInit, OnChanges {
   @Input() username:string;
   messages: Message[];
+  user: User;
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private route: ActivatedRoute, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.route.data.subscribe(data => {
+      this.user = this.authService.currentUser;
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    // console.log(changes);
+    // this.username = changes.username.currentValue;
     this.loadMessages();
+    this.route.data.subscribe(data => {
+      this.user = this.authService.currentUser;
+    });
+    console.log(this.user.username);
   }
 
   loadMessages() {
